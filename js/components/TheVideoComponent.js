aff622cb69ffea89ef3db4ff28d2d29ea99c1fcb
tvanrys <tvanrys@fanshaweonline.ca>
2020-12-03 13:26:35 -0500

    added sass to our components

--- js/components/TheVideoComponent.js
+++ js/components/TheVideoComponent.js
@@ -2,8 +2,11 @@ export default {
     name: "VideoComponent",
 
     template: `
-         <section>
+         <section class="video-player">
             <i class="fas fa-film media-icon"></i></i><h1>This is the video component</h1>
-        </section>
-    `
+            <video src="" controls></video>
+            <h3>Video Title</h3>
+            <h3>Video Release Date</h3>
+            <p>video description here...</p>
+        </section>`
 }
--- js/main.js
+++ js/main.js
@@ -4,9 +4,22 @@ import VideoComponent from "./components/TheVideoComponent.js";
 (() => {
     const myVM = new Vue({
         data: {
+            activeComponent: AudioComponent
         },
 
         methods: {
+            switchComponents() {
+                // debugger;
+
+                this.activeComponent = (this.activeComponent.name === "VideoComponent") ? AudioComponent : VideoComponent;
+
+                // this.doSomethingElse();
+            },
+
+            doSomethingElse() {
+                // whatever
+                debugger;
+            }
         }
     }).$mount("#app");
 })();
